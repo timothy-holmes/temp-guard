3edd94827f9e8fdba1f68e72f27af685821b31c7
Timothy Holmes <tim.a.holmes@gmail.com>
2022-05-23 12:56:51 +1000

    typed mock for WMI class

--- app/tests/mockforWMI.py
+++ app/tests/mockforWMI.py
@@ -1,21 +1,22 @@
 from random import randint
-
-class MockWMI:
-    def __init__(self,namespace):
-        self.namespace = namespace
-
-    def __repr__(self):
-        return f'<MockWMI {self.namespace}>'
-    
-    @staticmethod
-    def MSAcpi_ThermalZoneTemperature():
-        return [MockTempResponse()]
+from typing import List
 
 class MockTempResponse:
-    def __init__(self):
+    def __init__(self) -> None:
         values = {'ActiveTripPoint': 2732 + 500,
                 'PassiveTripPoint': 2732 + 1000,
                 'CriticalTripPoint': 2732 + 1500,
                 'CurrentTemperature': randint(2732, 2732 + 2000)}
         for property,property_value in values.items():
             setattr(self,property,property_value)
+
+class MockWMI:
+    def __init__(self,namespace) -> None: 
+        self.namespace = namespace
+
+    def __repr__(self) -> str:
+        return f'<MockWMI {self.namespace}>'
+    
+    @staticmethod
+    def MSAcpi_ThermalZoneTemperature() -> List[MockTempResponse]:
+        return [MockTempResponse()]
