bea732f479c4552771228219ec2af9274e1bb6da
Timothy Holmes <tim.a.holmes@gmail.com>
2022-06-13 20:52:40 +1000

    added check for admin rights

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,32 @@ from flask import Flask, jsonify, render_template, Response
 import wmi
 
 import datetime, os
+import ctypes
+import pythoncom
 
 app = Flask(__name__)
-w = wmi.WMI(namespace = r"root\wmi")
 
-def get_temp(property: str) -> Optional[int]:
+def am_i_admin():
     try:
-        w_class = w.MSAcpi_ThermalZoneTemperature()[0]
-        temp = getattr(w_class,property,None)
-    except Exception as e: # this is bad practice, TODO: isolate exception types
-        temp = None
-        print('get_temp error',e)
+         is_admin = os.getuid() == 0
+    except AttributeError:
+         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin == True
 
+def get_temp(attr: str = 'CurrentTemperature') -> Optional[int]:
+    # try:
+    w0 = wmi.WMI(namespace=r'root\wmi').MSAcpi_ThermalZoneTemperature()[0]
+    temp = getattr(w0,attr,None)
+
+    # except Exception as e: # this is bad practice, TODO: isolate exception types
+        # temp = None
+        # print('get_temp error',e)
+        # for p in dir(e):
+        #     if not p.startswith('_'):
+        #         print(property,getattr(e,p))
+    
     # convert from deci-degrees Kelvin to deci-degrees Celcius
-    if not temp is None:
-        temp = temp - 2732
-    return temp
+    return temp  - 2732
 
 @app.route('/')
 def index() -> str:
@@ -33,13 +43,14 @@ def get_current_temp() -> Response:
 
 @app.route('/trip-points')
 def get_trip_point() -> Response:
-    class_properties = ['ActiveTripPoint','PassiveTripPoint','CriticalTripPoint']    
-    trip_points = {property: get_temp(property) for property in class_properties}
+    class_attrs = ['ActiveTripPoint','PassiveTripPoint','CriticalTripPoint']    
+    trip_points = {a: get_temp(a) for a in class_attrs}
     return jsonify(trip_points)
 
 if __name__ == '__main__':
-    debug = False # use True for integration testing, TODO: move to .env
+    debug = False # use True for integration testing, TODO: move to a .env
     if debug:
         from tests.mockforWMI import MockWMI
         w = MockWMI(namespace = 'test WMI')
-    app.run(host = '127.0.0.1', port = 4999, debug = debug)
+    if am_i_admin():
+        app.run(host = '127.0.0.1', port = 4999, debug = debug)
